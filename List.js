6bee24d86882e7545971758dfc28f13ea4f1850b
DollyChouhan <79242686+DollyChouhan@users.noreply.github.com>
2021-03-09 15:43:45 +0530

    Add files via upload

---
+++ List.js
@@ -0,0 +1,24 @@
+import React from 'react';
+const List = (props) => {
+    const { repos } = props;
+    if (!repos || repos.length === 0) return <p>No repos, sorry</p>;
+    return (
+        <ul>
+            <h2 className='list-head'>Available Public Repositories</h2>
+            {repos.map((repo) => {
+                return (
+                    <li key={repo.id} className='list'>
+                        <span className='repo-text'>{repo.UserId} </span>
+                        <span className='repo-text'>{repo.Username} </span>
+                        <span className='repo-text'>{repo.mobNo} </span>
+                        <span className='repo-text'>{repo.Location} </span>
+                        <span className='repo-text'>{repo.Position} </span>
+                        <span className='repo-description'>{repo.description}</span>
+                    </li>
+                    
+                );
+            })}
+        </ul>
+    );
+};
+export default List;
